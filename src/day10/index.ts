a38f83446b0fbd1db6c5e9400a6052dbf02bab9b
Julius Schmidt <newceptiondev@googlemail.com>
2021-12-10 12:13:47 +0100

    Pretty Version of Day ten

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,137 +1,111 @@
-import { test, readInput } from "../utils"
+import { readInput } from "../utils"
 import { splitToLines } from "../utils/readInput"
 
 const prepareInput = (rawInput: string) => rawInput
 
 const input = prepareInput(readInput())
 
+const mapToStackAndFindFirstIncorrectCharacter = (characters: string[]): { stack: string[], incorrectCharacter: string | undefined } => {
+  const stack: string[] = []
+  let incorrectCharacter: string | undefined = undefined
+
+  for (let i = 0; i < characters.length && !incorrectCharacter; i++) {
+    if (characters[i] === "(" || characters[i] === "[" || characters[i] === "{" || characters[i] === "<") {
+      stack.push(characters[i])
+    } else {
+      switch (characters[i]) {
+        case ")":
+          if (stack.pop() !== "(") {
+            incorrectCharacter = characters[i]
+          }
+          break
+        case "]":
+          if (stack.pop() !== "[") {
+            incorrectCharacter = characters[i]
+          }
+          break
+        case "}":
+          if (stack.pop() !== "{") {
+            incorrectCharacter = characters[i]
+          }
+          break
+        case ">":
+          if (stack.pop() !== "<") {
+            incorrectCharacter = characters[i]
+          }
+          break
+      }
+    }
+  }
+
+  return { stack: stack, incorrectCharacter: incorrectCharacter }
+}
+
 const calculatePoints = (incorrectCharacters: Map<string, number>): number => {
   return Array.from(incorrectCharacters.keys()).map(key => {
     switch (key) {
       case ")":
-        return 3 * incorrectCharacters.get(key);
+        return 3 * incorrectCharacters.get(key)
       case "]":
-        return 57 * incorrectCharacters.get(key);
+        return 57 * incorrectCharacters.get(key)
       case "}":
-        return 1197 * incorrectCharacters.get(key);
+        return 1197 * incorrectCharacters.get(key)
       case ">":
-        return 25137 * incorrectCharacters.get(key);
+        return 25137 * incorrectCharacters.get(key)
     }
   }).reduce((previousValue, currentValue) => previousValue + currentValue, 0)
 }
 
 const goA = (input) => {
-  const lines = splitToLines(input);
+  const lines = splitToLines(input)
 
-  const incorrectCharacters: Map<string, number> = new Map<string, number>();
+  const incorrectCharacters: Map<string, number> = new Map<string, number>()
 
-  for(let line of lines) {
+  for (let line of lines) {
     const chars: string[] = Array.from(line)
-    const stack: string[] = [];
 
-    let incorrectCharacter: string | undefined = undefined;
+    const { incorrectCharacter } = mapToStackAndFindFirstIncorrectCharacter(chars)
 
-    for(let i = 0; i < chars.length && !incorrectCharacter; i++){
-      if(chars[i] === "(" || chars[i] === "[" || chars[i] === "{" || chars[i] === "<") {
-        stack.push(chars[i]);
-      } else {
-        switch (chars[i]) {
-          case ")":
-            if(stack.pop() !== "(") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case "]":
-            if(stack.pop() !== "[") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case "}":
-            if(stack.pop() !== "{") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case ">":
-            if(stack.pop() !== "<") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-        }
-      }
-    }
-
-    if(incorrectCharacter){
-      incorrectCharacters.set(incorrectCharacter, incorrectCharacters.has(incorrectCharacter) ? incorrectCharacters.get(incorrectCharacter) + 1 : 1);
+    if (incorrectCharacter) {
+      incorrectCharacters.set(incorrectCharacter, incorrectCharacters.has(incorrectCharacter) ? incorrectCharacters.get(incorrectCharacter) + 1 : 1)
     }
   }
 
-  return calculatePoints(incorrectCharacters);
+  return calculatePoints(incorrectCharacters)
 }
 
 const goB = (input) => {
-  const lines = splitToLines(input);
+  const lines = splitToLines(input)
 
-  const pointsForLines: number[] = [];
+  const pointsForLines: number[] = []
 
-  for(let line of lines) {
+  for (let line of lines) {
     const chars: string[] = Array.from(line)
-    const stack: string[] = [];
+    let points: number = 0
 
-    let points: number = 0;
+    const { stack, incorrectCharacter } = mapToStackAndFindFirstIncorrectCharacter(chars)
 
-    let incorrectCharacter: string | undefined = undefined;
-
-    for(let i = 0; i < chars.length && !incorrectCharacter; i++){
-      if(chars[i] === "(" || chars[i] === "[" || chars[i] === "{" || chars[i] === "<") {
-        stack.push(chars[i]);
-      } else {
-        switch (chars[i]) {
-          case ")":
-            if(stack.pop() !== "(") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case "]":
-            if(stack.pop() !== "[") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case "}":
-            if(stack.pop() !== "{") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-          case ">":
-            if(stack.pop() !== "<") {
-              incorrectCharacter = chars[i];
-            }
-            break;
-        }
-      }
-    }
-
-    if(!incorrectCharacter){
-      while(stack.length > 0){
-        const toClose: string = stack.pop();
-        points *= 5;
+    if (!incorrectCharacter) {
+      while (stack.length > 0) {
+        const toClose: string = stack.pop()
+        points *= 5
         switch (toClose) {
           case "(":
-            points += 1;
-            break;
+            points += 1
+            break
           case "[":
-            points += 2;
-            break;
+            points += 2
+            break
           case "{":
-            points += 3;
-            break;
+            points += 3
+            break
           case "<":
-            points += 4;
-            break;
-
+            points += 4
+            break
         }
       }
 
-      pointsForLines.push(points);
+      pointsForLines.push(points)
     }
   }
 
